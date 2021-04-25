9fc32b1971a65e01dde777342f23492444b8fde0
Pawel214 <9pixel9@gmail.com>
2021-04-25 19:19:02 +0200

    Add address table.

--- SQL/Library_DB_DDL.sql
+++ SQL/Library_DB_DDL.sql
@@ -1,6 +1,16 @@
+--
+-- Drop objects 
+--
 DROP SEQUENCE IF EXISTS person_seq;
-DROP TABLE IF EXISTS person;
+DROP TABLE IF EXISTS person CASCADE;
 
+DROP SEQUENCE IF EXISTS address_seq;
+DROP TABLE IF EXISTS address CASCADE;
+
+
+--
+-- Create objects 
+--
 CREATE SEQUENCE person_seq START 1 CACHE 20;
 CREATE TABLE person (
     id                   BIGINT NOT NULL,
@@ -19,3 +29,23 @@ CREATE TABLE person (
   CONSTRAINT person_uk1 UNIQUE (email) 
 );
 
+
+CREATE SEQUENCE address_seq START 1 CACHE 20;
+CREATE TABLE address (
+    id                   BIGINT NOT NULL,
+	person_id            BIGINT NOT NULL,
+	city_village         VARCHAR(64) NOT NULL,
+	street               VARCHAR(64) NOT NULL,
+	building_number      VARCHAR(8) NOT NULL,
+	apartment_number     VARCHAR(8),
+	zip_code             VARCHAR(16) NOT NULL,
+	created_by           VARCHAR(64) NOT NULL,
+    created_date         TIMESTAMP WITH TIME ZONE NOT NULL,  
+    last_updated_by      VARCHAR(64),
+    last_updated_date    TIMESTAMP WITH TIME ZONE,  
+    version              BIGINT NOT NULL DEFAULT 0,
+  CONSTRAINT address_pk PRIMARY KEY (id),
+  CONSTRAINT person_fk1 FOREIGN KEY(person_id) REFERENCES person(id)
+);
+
+CREATE INDEX address_idx1 ON address (person_id);
